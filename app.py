5bf1621593910ef98b908efac5f1a75a56222200
JayAccolite <jayavinay.namgiri@accolitedigital.com>
2022-04-18 10:55:10 +0530

    some message

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+from sys import platform
+
 import streamlit as st
 import numpy as np
 import PIL.Image
 import pandas as pd
 import os
 from fastai.vision.all import Path,load_learner,Image
-
+import pathlib
+plt = platform.system()
+if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
 path = Path('export.pkl')
 
